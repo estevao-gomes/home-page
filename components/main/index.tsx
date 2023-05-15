169cbd47f1ec88a39426c8c5b71c1c8a59451a12
Estevão <estevaog.gomes@gmail.com>
2023-05-15 20:04:12 -0300

    [Feature]Revamp

--- components/main/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-interface MainProps {
-  children: ReactElement[];
-}
-
-export function Main({ children }: MainProps) {
-  return <main className="">{children}</main>;
-}
