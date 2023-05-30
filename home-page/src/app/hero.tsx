f8e8340fb9a1b4b9744d69127bf0a2a4c6813826
Estevão <estevaog.gomes@gmail.com>
2023-05-29 21:11:28 -0300

    [Feature]Starts implementing Hero

---
+++ home-page/src/app/hero.tsx
@@ -0,0 +1,8 @@
+export function Hero() {
+  return (
+    <section className="h-screen">
+      <div className="h-full w-1/3"></div>
+      <div className="h-full w-2/3"></div>Hero section
+    </section>
+  );
+}
