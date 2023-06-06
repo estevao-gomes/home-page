5b111c74ffb0bc9947d1b6ec5bb09195a99977b9
Estevão Gomes <estevaog.gomes@gmail.com>
2023-06-06 14:55:38 +0000

    WIP

--- home-page/src/app/projects.tsx
+++ home-page/src/app/projects.tsx
@@ -1,13 +1,16 @@
 import { Project } from "@/components/Project";
 
+import websiteDesktop from "@/assets/websiteDesktop.png"
+import websiteMobile from "@/assets/websiteMobile.png"
+
 export function Projects(){
     return (
         <section className="">
             <h2 className="font-bold font-openSans text-3xl sm:text-5xl max-w-[75%]">Projects</h2>
             <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, dignissimos.</h4>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mb-8 mx-8">
-                <Project />
-                <Project />
+                <Project mobileSrc={websiteMobile} desktopSrc={websiteDesktop}/>
+                <Project mobileSrc={websiteMobile} desktopSrc={websiteDesktop}/>
             </div>
         </section>
     )
--- home-page/src/components/Project/index.tsx
+++ home-page/src/components/Project/index.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image'
-
+import Image, { StaticImageData } from 'next/image'
 import desktop from '@/assets/desktop.png'
 import mobile from '@/assets/mobile1.png'
 
-export function Project(){
+export function Project({desktopSrc, mobileSrc}:{desktopSrc:StaticImageData, mobileSrc:StaticImageData}){
+
     return(
         <div className='relative'>
-            <Image src={desktop} alt="Image of a desktop"/>
+            <Image src={desktop} width={500} height={300} alt="Image of a desktop"/>
             <Image src={mobile} alt="Image of a desktop" className='absolute bottom-0 right-10 z-10 max-w-[25%]'/>
+            <Image src={desktopSrc} alt="Image of project site" className='absolute z-20 top-0'/>
+            <Image src={mobileSrc} alt="Image of project site on mobile" className='absolute z-20 bottom-0 right-0' />
         </div>
     )
 }
